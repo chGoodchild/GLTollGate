b5323451f960dee56d8357cba99f126be2d8f7ee
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-06-06 17:56:31 +0200

    Simplified default example

--- v4.3.11/www/cgi-bin/payment.sh
+++ v4.3.11/www/cgi-bin/payment.sh
@@ -7,15 +7,8 @@ case "$METHOD" in
   auth_client)
     USERNAME="$3"
     PASSWORD="$4"
-    if [ "$USERNAME" = "Bill" -a "$PASSWORD" = "tms" ]; then
-      # Allow client to access the Internet for one hour (3600 seconds)
-      # Further values are upload and download limits in bytes. 0 for no limit.
-      echo 3600 0 0
-      exit 0
-    else
-      # Deny client to access the Internet.
-      exit 1
-    fi
+    echo 3600 0 0
+    exit 0
     ;;
   client_auth|client_deauth|idle_deauth|timeout_deauth|ndsctl_auth|ndsctl_deauth|shutdown_deauth)
     INGOING_BYTES="$3"
