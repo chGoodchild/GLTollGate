a576541f96d7e1efa7370c1b22617e512291abce
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-06-10 15:07:15 +0200

    Tried fixing payment.sh arguments

--- v4.3.11/www/cgi-bin/payment.sh
+++ v4.3.11/www/cgi-bin/payment.sh
@@ -2,13 +2,12 @@
 
 METHOD="$1"
 MAC="$2"
-ARG3="$3"
-ARG4="$4"
-ARG5="$5"
-ARG6="$6"
 
-# Log all arguments to /tmp/arguments_log.md
-echo "METHOD: $METHOD, MAC: $MAC, ARG3: $ARG3, ARG4: $ARG4, ARG5: $ARG5, ARG6: $ARG6" >> /tmp/arguments_log.md
+INGOING_BYTES="$3"
+OUTGOING_BYTES="$4"
+SESSION_START="$5"
+SESSION_END="$6"
+echo "METHOD: $METHOD, MAC: $MAC, INGOING_BYTES: $INGOING_BYTES, OUTGOING_BYTES: $OUTGOING_BYTES, SESSION_START: $SESSION_START, SESSION_END: $SESSION_END" >> /tmp/arguments_log.md
 
 case "$METHOD" in
   auth_client)
@@ -22,13 +21,7 @@ case "$METHOD" in
     OUTGOING_BYTES="$4"
     SESSION_START="$5"
     SESSION_END="$6"
-    # client_auth: Client authenticated via this script.
-    # client_deauth: Client deauthenticated by the client via splash page.
-    # idle_deauth: Client was deauthenticated because of inactivity.
-    # timeout_deauth: Client was deauthenticated because the session timed out.
-    # ndsctl_auth: Client was authenticated by the ndsctl tool.
-    # ndsctl_deauth: Client was deauthenticated by the ndsctl tool.
-    # shutdown_deauth: Client was deauthenticated by Nodogsplash terminating.
+    # Log the details of client actions
     echo "METHOD: $METHOD, MAC: $MAC, INGOING_BYTES: $INGOING_BYTES, OUTGOING_BYTES: $OUTGOING_BYTES, SESSION_START: $SESSION_START, SESSION_END: $SESSION_END" >> /tmp/arguments_log.md
     ;;
 esac
