68dbed868521d3700225c1a0d6025df345604abf
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-06-25 03:20:08 +0200

    wrtbmon also not producing the expected results

--- download_mbs.sh
+++ download_mbs.sh
@@ -5,6 +5,7 @@ ROUTER_IP="192.168.8.1"
 ROUTER_USER="root"
 ROUTER_PASSWORD="1"  # replace with your actual password
 IPERF_SERVER="178.18.252.85"  # Replace with your iperf3 server IP
+DATABASE_FILE="/tmp/usage.db"
 
 # Check if the number of MB to be downloaded is provided
 if [ -z "$1" ]; then
@@ -16,13 +17,20 @@ fi
 NUM_MB=$1
 NUM_BYTES=$((NUM_MB * 1000000))  # Convert MB to Bytes
 
-# Function to get downloaded and uploaded data from ndsctl json
-get_data_usage() {
-  sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "ndsctl json" | jq '.clients[] | .downloaded, .uploaded'
+# Function to setup and update wrtbwmon
+setup_wrtbwmon() {
+  sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "wrtbwmon setup $DATABASE_FILE"
+  sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "wrtbwmon update $DATABASE_FILE"
 }
 
-# Retrieve initial data usage from ndsctl
-initial_data=$(get_data_usage)
+# Function to get data usage from wrtbwmon
+get_wrtbwmon_data() {
+  sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "cat $DATABASE_FILE" | grep "28:d2:44:64:f1:f7" | awk -F',' '{print $4, $5}'
+}
+
+# Setup wrtbwmon and retrieve initial data usage
+setup_wrtbwmon
+initial_data=$(get_wrtbwmon_data)
 initial_downloaded=$(echo $initial_data | awk '{print $1}')
 initial_uploaded=$(echo $initial_data | awk '{print $2}')
 
@@ -30,8 +38,9 @@ initial_uploaded=$(echo $initial_data | awk '{print $2}')
 echo "Running iperf3 to download $NUM_MB MB"
 iperf3 -c $IPERF_SERVER -n $NUM_BYTES
 
-# Retrieve final data usage from ndsctl
-final_data=$(get_data_usage)
+# Update wrtbwmon and retrieve final data usage
+setup_wrtbwmon
+final_data=$(get_wrtbwmon_data)
 final_downloaded=$(echo $final_data | awk '{print $1}')
 final_uploaded=$(echo $final_data | awk '{print $2}')
 
@@ -40,6 +49,6 @@ downloaded_consumed=$((final_downloaded - initial_downloaded))
 uploaded_consumed=$((final_uploaded - initial_uploaded))
 
 echo "Expected data download: $NUM_MB MB."
-echo "Actual data downloaded (ndsctl): $((downloaded_consumed)) MB."
-echo "Actual data uploaded (ndsctl): $((uploaded_consumed)) MB."
+echo "Actual data downloaded (wrtbwmon): $((downloaded_consumed / 1024)) MB."
+echo "Actual data uploaded (wrtbwmon): $((uploaded_consumed / 1024)) MB."
 
