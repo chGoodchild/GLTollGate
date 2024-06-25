ee35a7a182db5f6708c3e8ea51e2f933d57d6498
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-06-25 20:47:11 +0200

    Setup successfully

--- setup.sh
+++ setup.sh
@@ -11,6 +11,13 @@ MARKER_DIR="/tmp/markers"
 # Ensure marker directory exists
 sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "mkdir -p $MARKER_DIR"
 
+# Step 4: Copy CGI scripts and make them executable
+if ! sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "[ -f $MARKER_DIR/cgi_scripts_copied ]"; then
+    sshpass -p "$ROUTER_PASSWORD" scp -r www/cgi-bin/*.sh $ROUTER_USER@$ROUTER_IP:/www/cgi-bin/.
+    sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "chmod +x /www/cgi-bin/*.sh"
+    sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "touch $MARKER_DIR/cgi_scripts_copied"
+fi
+
 # Step 1: Copy the nodogsplash package and install it
 if ! sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "[ -f $MARKER_DIR/nodogsplash_installed ]"; then
     sshpass -p "$ROUTER_PASSWORD" scp nodogsplash_5.0.0-1_mips_24kc.ipk $ROUTER_USER@$ROUTER_IP:/tmp/
@@ -23,13 +30,6 @@ service nodogsplash status
 logread | grep nodogsplash
 touch /tmp/markers/nodogsplash_installed
 ENDSSH
-fi
-
-# Step 4: Copy CGI scripts and make them executable
-if ! sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "[ -f $MARKER_DIR/cgi_scripts_copied ]"; then
-    sshpass -p "$ROUTER_PASSWORD" scp -r www/cgi-bin/*.sh $ROUTER_USER@$ROUTER_IP:/www/cgi-bin/.
-    sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "chmod +x /www/cgi-bin/*.sh"
-    sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "touch $MARKER_DIR/cgi_scripts_copied"
 fi
 
 # Step 2: Install additional packages
@@ -60,5 +60,6 @@ fi
 if ! sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "[ -f $MARKER_DIR/nodogsplash_dir_copied ]"; then
     sshpass -p "$ROUTER_PASSWORD" scp -r etc/nodogsplash/ $ROUTER_USER@$ROUTER_IP:/etc/.
     sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "touch $MARKER_DIR/nodogsplash_dir_copied"
+    sshpass -p "$ROUTER_PASSWORD" ssh $ROUTER_USER@$ROUTER_IP "service nodogsplash restart"
 fi
 
