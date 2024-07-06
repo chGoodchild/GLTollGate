073249cf0579b3d2d82efadabeab6867e7d4919f
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-06 12:23:52 +0200

    Notes still not accepted

--- nostr/note_generator.sh
+++ nostr/note_generator.sh
@@ -33,24 +33,11 @@ if [ ! -f "$PEM_FILE" ]; then
     exit 1
 fi
 
-# Ensure private key hex is in the correct format
-PRIVATE_KEY_HEX_CLEAN=$(echo -n $PRIVATE_KEY_HEX | xxd -r -p | base64 | tr -d '\n')
-
-# Create a PEM file from the private key hex
-echo "-----BEGIN EC PRIVATE KEY-----
-MHQCAQEEIF${PRIVATE_KEY_HEX_CLEAN}AgEBME8w
------END EC PRIVATE KEY-----" > temp.pem
-
 # Sign the serialized event using the PEM file
-SIGNATURE=$(echo -n "$SERIALIZED_EVENT" | openssl dgst -sha256 -sign temp.pem | openssl base64 | tr -d '\n')
-# SIGNATURE=$(echo -n "$SERIALIZED_EVENT" | openssl dgst -sha256 -sign "$PEM_FILE" | openssl base64 | tr -d '\n')
-
+SIGNATURE=$(echo -n "$SERIALIZED_EVENT" | openssl dgst -sha256 -sign "$PEM_FILE" | openssl dgst -sha256 -binary | openssl base64 | tr -d '\n')
 
 # Compute the event ID (hash of the serialized event)
 EVENT_ID=$(echo -n "$SERIALIZED_EVENT" | openssl dgst -sha256 | awk '{print $2}')
-
-# Clean up the temporary PEM file
-#rm temp.pem
 
 # Update the event with the ID and signature
 EVENT=$(echo "$EVENT" | jq --arg id "$EVENT_ID" --arg sig "$SIGNATURE" '.id = $id | .sig = $sig')
