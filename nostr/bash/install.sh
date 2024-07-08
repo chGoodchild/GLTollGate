fe1333020be4de3dd20573fbbd365b3aac8badb3
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-08 15:42:10 +0200

    More cleanup

--- nostr/bash/install.sh
+++ nostr/bash/install.sh
@@ -31,3 +31,28 @@ sudo mv $WEBSOCAT_BIN $INSTALL_DIR
 
 echo "Websocat installed successfully."
 
+# Update package list and install required dependencies
+sudo apt-get update
+sudo apt-get install -y autoconf automake libtool git
+
+# Clone the secp256k1 repository into /tmp
+cd /tmp
+git clone https://github.com/bitcoin-core/secp256k1.git
+cd secp256k1
+
+# Run the autogen script
+./autogen.sh
+
+# Configure the build with Schnorr signatures and ECDH enabled
+./configure --enable-module-schnorrsig --enable-module-ecdh
+
+# Build the library
+make
+
+# Optionally run the test suite
+make check
+
+# Optionally install the library
+sudo make install
+
+echo "secp256k1 library has been built and installed."
