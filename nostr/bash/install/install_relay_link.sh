3bd924d1193b3e2260864bce1660ebfad8b9da9b
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-18 23:21:26 +0200

    RelayLink in /tmp at all times

--- nostr/bash/install/install_relay_link.sh
+++ nostr/bash/install/install_relay_link.sh
@@ -7,7 +7,6 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ARCH=$(uname -m)
 CHECKSUMS_FILE="/tmp/checksums.json"
 CHECKSUMS_URL="https://github.com/chGoodchild/TollGateNostrToolKit/releases/download/v0.0.5/checksums.json"
-INSTALL_DIR="$SCRIPT_DIR/.."
 
 # Download the checksums file if it doesn't exist
 if [ ! -f "$CHECKSUMS_FILE" ]; then
@@ -19,14 +18,10 @@ case $ARCH in
     x86_64)
         BINARY_URL="https://github.com/chGoodchild/TollGateNostrToolKit/releases/download/v0.0.5/RelayLink"
         BINARY_PATH="/tmp/RelayLink"
-        TARGET_BINARY_PATH="$INSTALL_DIR/RelayLink"
-        EXPECTED_HASH=$(jq -r '.x86_64_binary_checksum' $CHECKSUMS_FILE)
         ;;
     mips)
         BINARY_URL="https://github.com/chGoodchild/TollGateNostrToolKit/releases/download/v0.0.5/RelayLink_mips"
         BINARY_PATH="/tmp/RelayLink_mips"
-        TARGET_BINARY_PATH="$INSTALL_DIR/RelayLink_mips"
-        EXPECTED_HASH=$(jq -r '.mips_binary_checksum' $CHECKSUMS_FILE)
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
@@ -40,21 +35,20 @@ download_and_verify_binary() {
     wget -q $BINARY_URL -O $BINARY_PATH
     CURRENT_HASH=$(sha256sum $BINARY_PATH | awk '{ print $1 }')
     
-    if [ "$CURRENT_HASH" != "$EXPECTED_HASH" ]; then
+    if [ "$CURRENT_HASH" != "$(jq -r ".$(basename $BINARY_PATH)_checksum" $CHECKSUMS_FILE)" ]; then
         echo "Error: Checksum verification failed."
         exit 1
     fi
 
     chmod +x $BINARY_PATH
-    mv $BINARY_PATH $TARGET_BINARY_PATH
-    echo "Installation and verification complete. $TARGET_BINARY_PATH is ready to use."
+    echo "Installation and verification complete. $BINARY_PATH is ready to use."
 }
 
-# Check if the binary is already in the install directory and has the correct checksum
-if [ -f "$TARGET_BINARY_PATH" ]; then
-    CURRENT_HASH=$(sha256sum $TARGET_BINARY_PATH | awk '{ print $1 }')
-    if [ "$CURRENT_HASH" = "$EXPECTED_HASH" ]; then
-        echo "$TARGET_BINARY_PATH is up to date."
+# Check if the binary is already in the tmp directory and has the correct checksum
+if [ -f "$BINARY_PATH" ]; then
+    CURRENT_HASH=$(sha256sum $BINARY_PATH | awk '{ print $1 }')
+    if [ "$CURRENT_HASH" = "$(jq -r ".$(basename $BINARY_PATH)_checksum" $CHECKSUMS_FILE)" ]; then
+        echo "$BINARY_PATH is up to date."
         exit 0
     else
         download_and_verify_binary
@@ -62,6 +56,4 @@ if [ -f "$TARGET_BINARY_PATH" ]; then
 else
     download_and_verify_binary
 fi
-
-
 
