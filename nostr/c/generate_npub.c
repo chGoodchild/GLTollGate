21a1767b34eeea82bc958c3f036ac8afa8dc72f7
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-27 18:40:55 +0200

    Produces a BIP39 seed phrase
    $ ./generate_npub
    {
      "npub": "-----BEGIN PUBLIC KEY-----
    MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEys8I/xyhy2cqgnT7X/903aZWpP4jammb
    uGCDqo63Hug8ppw6kh/uV4ks4Z1u1+NO2jBWlWOkDLovLtzRLIGtgA==
    -----END PUBLIC KEY-----
    ",
      "nsec": "-----BEGIN PRIVATE KEY-----
    MIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQgCz2GFnotzljVg1gi0uAh
    NJTaG9hEkI18sjlM9AgIEgOhRANCAATKzwj/HKHLZyqCdPtf/3Tdplak/iNqaZu4
    YIOqjrce6DymnDqSH+5XiSzhnW7X407aMFaVY6QMui8u3NEsga2A
    -----END PRIVATE KEY-----
    ",
      "bip39_nsec": "airport faint pistol scorpion blouse quote broccoli camp search banner shove equip web clay orange curious write jewel tired material enact habit solar derive"
    }

--- nostr/c/generate_npub.c
+++ nostr/c/generate_npub.c
@@ -15,53 +15,70 @@ int generate_ecdsa_keypair() {
 
     EVP_PKEY *pkey = NULL;
     EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
-    if (!(pctx && EVP_PKEY_keygen_init(pctx))) {
-        fprintf(stderr, "Failed to initialize key generation context.\n");
+    if (pctx == NULL) {
+        fprintf(stderr, "Failed to create EVP_PKEY_CTX.\n");
         return 1;
     }
 
-    pkey = EVP_PKEY_new();
-    if (!EVP_PKEY_generate(pctx, &pkey)) {
-        fprintf(stderr, "Failed to generate key.\n");
+    if (EVP_PKEY_keygen_init(pctx) != 1) {
+        fprintf(stderr, "Failed to initialize key generation context.\n");
         EVP_PKEY_CTX_free(pctx);
         return 1;
     }
 
-    // Memory streams for public and private keys
+    if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, NID_secp256k1) != 1) {
+        fprintf(stderr, "Failed to set EC curve.\n");
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
+    }
+
+    if (EVP_PKEY_keygen(pctx, &pkey) != 1) {
+        fprintf(stderr, "Failed to generate EC key.\n");
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
+    }
+
     unsigned char *pubkey = NULL, *privkey = NULL;
     size_t pubkey_len = 0, privkey_len = 0;
     FILE *pub_fp = open_memstream((char **)&pubkey, &pubkey_len);
     FILE *priv_fp = open_memstream((char **)&privkey, &privkey_len);
+
+    if (pub_fp == NULL || priv_fp == NULL) {
+        fprintf(stderr, "Failed to create memory streams.\n");
+        if (pub_fp) fclose(pub_fp);
+        if (priv_fp) fclose(priv_fp);
+        EVP_PKEY_free(pkey);
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
+    }
 
     PEM_write_PUBKEY(pub_fp, pkey);
     PEM_write_PrivateKey(priv_fp, pkey, NULL, NULL, 0, NULL, NULL);
     fclose(pub_fp);
     fclose(priv_fp);
 
-    // Generate BIP-39 Mnemonic
     unsigned char entropy[32];
     char *mnemonic = NULL;
     if (RAND_bytes(entropy, sizeof(entropy)) != 1) {
         fprintf(stderr, "Failed to generate secure random bytes.\n");
-        goto end;
+        goto cleanup;
     }
     
     if (bip39_mnemonic_from_bytes(NULL, entropy, sizeof(entropy), &mnemonic) != WALLY_OK) {
         fprintf(stderr, "Failed to generate mnemonic.\n");
-        goto end;
+        goto cleanup;
     }
 
-    // Output in JSON format
     output_json((const char*)pubkey, (const char*)privkey, mnemonic);
 
-end:
+cleanup:
     free(pubkey);
     free(privkey);
     if (mnemonic) wally_free_string(mnemonic);
     if (pkey) EVP_PKEY_free(pkey);
     if (pctx) EVP_PKEY_CTX_free(pctx);
 
-    wally_cleanup(0);  // Cleanup libwally resources
+    wally_cleanup(0);
     return 0;
 }
 
