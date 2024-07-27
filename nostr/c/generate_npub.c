b58f28db2785b0c982be8b20670785371c7a8750
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-27 18:52:28 +0200

    Missing bech32.h
    gcc -c -o generate_npub.o generate_npub.c -I/usr/local/include -I/usr/include/openssl -g -Wall
    generate_npub.c:13:10: fatal error: bech32.h: No such file or directory
       13 | #include "bech32.h"
          |          ^~~~~~~~~~
    compilation terminated.
    make: *** [Makefile:15: generate_npub.o] Error 1
    p

--- nostr/c/generate_npub.c
+++ nostr/c/generate_npub.c
@@ -2,13 +2,23 @@
 #include <wally_bip39.h>
 #include <wally_address.h>
 #include <openssl/evp.h>
+#include <openssl/ec.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "bech32.h"
 
 void output_json(const char* npub, const char* nsec, const char* mnemonic);
+
+char *encode_bech32(unsigned char *data, size_t data_len) {
+    char *encoded;
+    bech32_encode(&encoded, "bc", data, data_len);
+    return encoded;
+}
 
 int generate_ecdsa_keypair() {
     wally_init(0);  // Initialize libwally
@@ -38,24 +48,25 @@ int generate_ecdsa_keypair() {
         return 1;
     }
 
-    unsigned char *pubkey = NULL, *privkey = NULL;
-    size_t pubkey_len = 0, privkey_len = 0;
-    FILE *pub_fp = open_memstream((char **)&pubkey, &pubkey_len);
-    FILE *priv_fp = open_memstream((char **)&privkey, &privkey_len);
+    // Extract the raw private key in DER format
+    BIO *priv_bio = BIO_new(BIO_s_mem());
+    PEM_write_bio_PrivateKey(priv_bio, pkey, NULL, NULL, 0, NULL, NULL);
+    size_t privkey_len = BIO_pending(priv_bio);
+    unsigned char *privkey = malloc(privkey_len);  // Allocate memory for private key
+    BIO_read(priv_bio, privkey, privkey_len);
+    BIO_free(priv_bio);
 
-    if (pub_fp == NULL || priv_fp == NULL) {
-        fprintf(stderr, "Failed to create memory streams.\n");
-        if (pub_fp) fclose(pub_fp);
-        if (priv_fp) fclose(priv_fp);
-        EVP_PKEY_free(pkey);
-        EVP_PKEY_CTX_free(pctx);
-        return 1;
-    }
+    // Extract the raw public key in DER format
+    BIO *pub_bio = BIO_new(BIO_s_mem());
+    PEM_write_bio_PUBKEY(pub_bio, pkey);
+    size_t pubkey_len = BIO_pending(pub_bio);
+    unsigned char *pubkey = malloc(pubkey_len);  // Allocate memory for public key
+    BIO_read(pub_bio, pubkey, pubkey_len);
+    BIO_free(pub_bio);
 
-    PEM_write_PUBKEY(pub_fp, pkey);
-    PEM_write_PrivateKey(priv_fp, pkey, NULL, NULL, 0, NULL, NULL);
-    fclose(pub_fp);
-    fclose(priv_fp);
+    // Convert both keys to Bech32
+    char *npub = encode_bech32(pubkey, pubkey_len);
+    char *nsec = encode_bech32(privkey, privkey_len);
 
     unsigned char entropy[32];
     char *mnemonic = NULL;
@@ -69,9 +80,11 @@ int generate_ecdsa_keypair() {
         goto cleanup;
     }
 
-    output_json((const char*)pubkey, (const char*)privkey, mnemonic);
+    output_json(npub, nsec, mnemonic);
 
 cleanup:
+    free(npub);
+    free(nsec);
     free(pubkey);
     free(privkey);
     if (mnemonic) wally_free_string(mnemonic);
@@ -83,11 +96,10 @@ cleanup:
 }
 
 int main() {
-    // Initialize OpenSSL algorithms
     OpenSSL_add_all_algorithms();
-
-    // Generate the keypair
-    return generate_ecdsa_keypair();
+    int result = generate_ecdsa_keypair();
+    EVP_cleanup();
+    return result;
 }
 
 void output_json(const char* npub, const char* nsec, const char* mnemonic) {
