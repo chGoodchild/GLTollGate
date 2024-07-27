c3f51561983ced2b0e84e48ddc8d3f08bd8892d2
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-27 11:50:19 +0200

    libbtc functions don't exist

--- nostr/c/generate_npub.c
+++ nostr/c/generate_npub.c
@@ -1,6 +1,4 @@
-// #include "~/libbtc/src/trezor-crypto/bip39.h"
 #include "/home/pachai/libbtc/src/trezor-crypto/bip39.h"
-// #include <wally_core.h>  // Adjust include based on actual Bech32 library used
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
@@ -8,15 +6,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Declare these functions if their implementations are not included in the headers
+void btc_init(void);
+void btc_cleanup(void);
+char* convert_to_bech32(const unsigned char* data, size_t len);
+const char* mnemonic_from_data(const uint8_t* data, int len);
+void output_json(const char* npub, const char* nsec, const char* mnemonic);
+
 void generate_ecdsa_keypair() {
     // Initialize libraries (if needed)
     btc_init();  // Initializes libbtc, needed for BIP-39
 
     EVP_PKEY *pkey = NULL;
     EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
+    if (!(pctx && EVP_PKEY_keygen_init(pctx))) {
+        fprintf(stderr, "Failed to initialize key generation context.\n");
+        return;
+    }
+
+    // Generate key
+    pkey = EVP_PKEY_new();
+    if (!EVP_PKEY_generate(pctx, &pkey)) {
+        fprintf(stderr, "Failed to generate key.\n");
+        EVP_PKEY_CTX_free(pctx);
+        return;
+    }
+
     unsigned char *pubkey = NULL, *privkey = NULL;
-    char *mnemonic = NULL;
-    char *bech32_pub = NULL, *bech32_priv = NULL;
     size_t pubkey_len = 0, privkey_len = 0;
     FILE *pub_fp = open_memstream((char **)&pubkey, &pubkey_len);
     FILE *priv_fp = open_memstream((char **)&privkey, &privkey_len);
@@ -27,9 +43,8 @@ void generate_ecdsa_keypair() {
     fclose(priv_fp);
 
     // Convert to Bech32
-    // Note: You will need to write or use a function that converts public and private key bytes to Bech32.
-    bech32_pub = convert_to_bech32(pubkey, pubkey_len);
-    bech32_priv = convert_to_bech32(privkey, privkey_len);
+    char *bech32_pub = convert_to_bech32(pubkey, pubkey_len);
+    char *bech32_priv = convert_to_bech32(privkey, privkey_len);
 
     // Generate BIP-39 Mnemonic
     unsigned char entropy[32];
@@ -37,7 +52,7 @@ void generate_ecdsa_keypair() {
         fprintf(stderr, "Failed to generate secure random bytes.\n");
         goto end;
     }
-    mnemonic = mnemonic_from_data(entropy, sizeof(entropy));
+    const char *mnemonic = mnemonic_from_data(entropy, sizeof(entropy));
 
     // Output in JSON format
     output_json(bech32_pub, bech32_priv, mnemonic);
@@ -45,7 +60,6 @@ void generate_ecdsa_keypair() {
 end:
     free(pubkey);
     free(privkey);
-    free(mnemonic);
     free(bech32_pub);
     free(bech32_priv);
     if (pkey) EVP_PKEY_free(pkey);
@@ -61,11 +75,6 @@ int main() {
     // Generate the keypair
     generate_ecdsa_keypair();
     return 0;
-}
-
-char* entropy_to_mnemonic(unsigned char *entropy, size_t size) {
-    // Implement or call your mnemonic generation library here
-    return strdup("example-mnemonic-phrase");
 }
 
 void output_json(const char* npub, const char* nsec, const char* mnemonic) {
