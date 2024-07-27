1d8fcb3ba63891b04ea2cf7c7bae8312b470ab9b
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-27 19:26:00 +0200

    Failed to get raw public key

--- nostr/c/generate_npub.c
+++ nostr/c/generate_npub.c
@@ -2,42 +2,27 @@
 #include <wally_bip39.h>
 #include <wally_address.h>
 #include <openssl/evp.h>
-#include <openssl/ec.h>
-#include <openssl/pem.h>
 #include <openssl/rand.h>
-#include <openssl/bio.h>
-#include <openssl/err.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "bech32.h"
 
 void output_json(const char* npub, const char* nsec, const char* mnemonic);
 
-char *encode_bech32(unsigned char *data, size_t data_len) {
-    char *encoded;
-    bech32_encode(&encoded, "bc", data, data_len);
-    return encoded;
-}
-
 int generate_ecdsa_keypair() {
-    wally_init(0);  // Initialize libwally
+    wally_init(0);
 
     EVP_PKEY *pkey = NULL;
     EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
-    if (pctx == NULL) {
+    if (!pctx) {
         fprintf(stderr, "Failed to create EVP_PKEY_CTX.\n");
         return 1;
     }
 
-    if (EVP_PKEY_keygen_init(pctx) != 1) {
-        fprintf(stderr, "Failed to initialize key generation context.\n");
-        EVP_PKEY_CTX_free(pctx);
-        return 1;
-    }
-
-    if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, NID_secp256k1) != 1) {
-        fprintf(stderr, "Failed to set EC curve.\n");
+    if (EVP_PKEY_keygen_init(pctx) != 1 || EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, NID_secp256k1) != 1) {
+        fprintf(stderr, "Failed to initialize EC key generation.\n");
         EVP_PKEY_CTX_free(pctx);
         return 1;
     }
@@ -48,49 +33,45 @@ int generate_ecdsa_keypair() {
         return 1;
     }
 
-    // Extract the raw private key in DER format
-    BIO *priv_bio = BIO_new(BIO_s_mem());
-    PEM_write_bio_PrivateKey(priv_bio, pkey, NULL, NULL, 0, NULL, NULL);
-    size_t privkey_len = BIO_pending(priv_bio);
-    unsigned char *privkey = malloc(privkey_len);  // Allocate memory for private key
-    BIO_read(priv_bio, privkey, privkey_len);
-    BIO_free(priv_bio);
+    // Use the EVP_PKEY directly to extract the public key bytes
+    size_t pubkey_len = 0;
+    EVP_PKEY_get_raw_public_key(pkey, NULL, &pubkey_len); // Get length first
+    unsigned char *pubkey = malloc(pubkey_len);
+    if (!EVP_PKEY_get_raw_public_key(pkey, pubkey, &pubkey_len)) {
+        fprintf(stderr, "Failed to get raw public key.\n");
+        free(pubkey);
+        EVP_PKEY_free(pkey);
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
+    }
 
-    // Extract the raw public key in DER format
-    BIO *pub_bio = BIO_new(BIO_s_mem());
-    PEM_write_bio_PUBKEY(pub_bio, pkey);
-    size_t pubkey_len = BIO_pending(pub_bio);
-    unsigned char *pubkey = malloc(pubkey_len);  // Allocate memory for public key
-    BIO_read(pub_bio, pubkey, pubkey_len);
-    BIO_free(pub_bio);
-
-    // Convert both keys to Bech32
-    char *npub = encode_bech32(pubkey, pubkey_len);
-    char *nsec = encode_bech32(privkey, privkey_len);
+    char *address = NULL;
+    if (wally_addr_segwit_from_bytes(pubkey, pubkey_len, "bc", 0, &address) != WALLY_OK) {
+        fprintf(stderr, "Failed to convert public key to Bech32 address.\n");
+        free(pubkey);
+        EVP_PKEY_free(pkey);
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
+    }
 
     unsigned char entropy[32];
     char *mnemonic = NULL;
-    if (RAND_bytes(entropy, sizeof(entropy)) != 1) {
-        fprintf(stderr, "Failed to generate secure random bytes.\n");
-        goto cleanup;
-    }
-    
-    if (bip39_mnemonic_from_bytes(NULL, entropy, sizeof(entropy), &mnemonic) != WALLY_OK) {
+    if (RAND_bytes(entropy, sizeof(entropy)) != 1 || bip39_mnemonic_from_bytes(NULL, entropy, sizeof(entropy), &mnemonic) != WALLY_OK) {
         fprintf(stderr, "Failed to generate mnemonic.\n");
-        goto cleanup;
+        if (mnemonic) wally_free_string(mnemonic);
+        free(pubkey);
+        EVP_PKEY_free(pkey);
+        EVP_PKEY_CTX_free(pctx);
+        return 1;
     }
 
-    output_json(npub, nsec, mnemonic);
+    output_json(address, "Private key not exposed", mnemonic);
 
-cleanup:
-    free(npub);
-    free(nsec);
+    free(address);
+    wally_free_string(mnemonic);
     free(pubkey);
-    free(privkey);
-    if (mnemonic) wally_free_string(mnemonic);
-    if (pkey) EVP_PKEY_free(pkey);
-    if (pctx) EVP_PKEY_CTX_free(pctx);
-
+    EVP_PKEY_free(pkey);
+    EVP_PKEY_CTX_free(pctx);
     wally_cleanup(0);
     return 0;
 }
