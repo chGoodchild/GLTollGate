2b9be454a91dd96d62a88dfb87859e239e7aa45b
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-31 13:52:45 +0200

    Updated install keygen

--- nostr/shell/install/install_keygen.sh
+++ nostr/shell/install/install_keygen.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
 
 # Define URLs and file paths
-PYTHON_SCRIPT_URL="https://github.com/chGoodchild/nostrKeys/releases/download/v0.0.3/generate_npub.py"
-PYTHON_SCRIPT="/tmp/generate_npub.py"
-EXPECTED_HASH="160113dc186ca34c4e905375f659b0c37790d62e2b9002dc842360daaa36f056"
+KEYGEN_SCRIPT_URL="https://github.com/chGoodchild/GLTollGate/releases/download/v0.0.3/generate_npub"
+KEYGEN_PROGRAM="/tmp/generate_npub"
+EXPECTED_HASH="002dfa4894998580538080e1be9e68e2b3bec89dbe226287566d04f124ec3994"
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 OUTPUT_FILE="$SCRIPT_DIR/../nostr_keys.json"
 
-# Check if Python is installed
-if ! command -v python3 > /dev/null; then
-    echo "Python3 is not installed. Please install Python3 to proceed."
-    exit 1
-fi
-
-# Function to download and verify the Python script
+# Function to download and verify the keygen script
 download_and_verify_script() {
-    echo "Downloading generate_npub.py..."
-    wget -q $PYTHON_SCRIPT_URL -O $PYTHON_SCRIPT
-    CURRENT_HASH=$(sha256sum $PYTHON_SCRIPT | awk '{ print $1 }')
+    echo "Downloading generate_npub..."
+    # Download the keygen script using curl with -L to follow redirects
+    curl -L -o $KEYGEN_PROGRAM $KEYGEN_SCRIPT_URL
 
+    # Calculate the current file's hash
+    CURRENT_HASH=$(sha256sum $KEYGEN_PROGRAM | awk '{ print $1 }')
+
+    # Verify the checksum
     if [ "$CURRENT_HASH" != "$EXPECTED_HASH" ]; then
         echo "Error: Checksum verification failed."
         exit 1
     fi
 
-    chmod +x $PYTHON_SCRIPT
+    # Make the keygen script executable
+    chmod +x $KEYGEN_PROGRAM
 }
 
-# Check if the Python script is already present and matches the expected SHA-256 hash
-if [ -f "$PYTHON_SCRIPT" ]; then
-    CURRENT_HASH=$(sha256sum $PYTHON_SCRIPT | awk '{ print $1 }')
+# Check if the keygen script is already present and matches the expected SHA-256 hash
+if [ -f "$KEYGEN_PROGRAM" ]; then
+    CURRENT_HASH=$(sha256sum $KEYGEN_PROGRAM | awk '{ print $1 }')
     if [ "$CURRENT_HASH" != "$EXPECTED_HASH" ]; then
-        echo "generate_npub.py is outdated or corrupted."
+        echo "generate_npub is outdated or corrupted. Downloading a fresh copy."
         download_and_verify_script
     else
-        echo "generate_npub.py is up to date."
+        echo "generate_npub is up to date."
     fi
 else
+    # If the script is not present, download it
     download_and_verify_script
 fi
 
