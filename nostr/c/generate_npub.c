64cdce58364d1e4be6db372abeee8d61480a5074
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-31 13:06:14 +0200

    Removed openssl stuff

--- nostr/c/generate_npub.c
+++ nostr/c/generate_npub.c
@@ -6,6 +6,7 @@
 #include <string.h>  // If you need memory functions like memset etc.
 #include <stddef.h>  // For NULL
 
+/**
 #include <openssl/evp.h>        // For EVP_PKEY, EVP_PKEY_CTX, etc.
 #include <openssl/pem.h>        // For PEM_read_PUBKEY, PEM_read_PrivateKey
 #include <openssl/rand.h>       // For RAND_bytes
@@ -15,30 +16,33 @@
 #include <openssl/core_names.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
+**/
 
-#include <wally_core.h>         // For libwally functions
+// #include <wally_core.h>         // For libwally functions
 #include <wally_bip39.h>        // For mnemonic generation
-#include <wally_address.h>      // Additional libwally functionality
+// #include <wally_address.h>      // Additional libwally functionality
 
 // Function Declarations
-void handle_errors();
-void output_json(const char* npub_hex, const char* nsec_hex, const char* mnemonic);
+//void handle_errors();
+void output_json(const char* npub_hex, const char* nsec_hex);
 char* to_hex(const unsigned char *data, int length);
 char* convert_key_to_hex(const char* filename, int is_public);  // Function declaration
 
 
+/**
 void handle_errors() {
     ERR_print_errors_fp(stderr);
     abort();
 }
+**/
 
-void output_json(const char* npub_hex, const char* nsec_hex, const char* mnemonic) {
+void output_json(const char* npub_hex, const char* nsec_hex) {
     printf("{\n");
     printf("  \"npub_hex\": \"%s\",\n", npub_hex);
     printf("  \"nsec_hex\": \"%s\",\n", nsec_hex);
-    printf("  \"bip39_nsec\": \"%s\"\n", mnemonic);
     printf("}\n");
 }
+
 
 char* to_hex(const unsigned char *data, int length) {
     char* hex_string = malloc(length * 2 + 1);
@@ -50,6 +54,7 @@ char* to_hex(const unsigned char *data, int length) {
     return hex_string;
 }
 
+/**
 char* convert_ec_public_key_to_hex(EVP_PKEY *pkey) {
     OSSL_ENCODER_CTX *ctx = NULL; // No need to pre-declare as NULL here when using new_for_pkey directly
     BIO *bio = BIO_new(BIO_s_mem());
@@ -118,7 +123,7 @@ char* convert_key_to_hex(const char* filename, int is_public) {
 
     return hex;
 }
-
+**/
 
 
 int generate_ecdsa_keypair() {
@@ -138,6 +143,7 @@ int generate_ecdsa_keypair() {
     char *pubkey_hex = to_hex(my_key.pubkey, 32);
     char *privkey_hex = to_hex(my_key.secret, 32);
 
+    /**
     unsigned char entropy[32];
     char *mnemonic = NULL;
     if (RAND_bytes(entropy, sizeof(entropy)) != 1) {
@@ -148,20 +154,20 @@ int generate_ecdsa_keypair() {
         fprintf(stderr, "Failed to generate mnemonic.\n");
         goto cleanup;
     }
-    output_json(pubkey_hex, privkey_hex, mnemonic);
+    **/
+    output_json(pubkey_hex, privkey_hex);
 
-cleanup:
     free(pubkey_hex);
     free(privkey_hex);
-    if (mnemonic) wally_free_string(mnemonic);
+    // if (mnemonic) wally_free_string(mnemonic);
     secp256k1_context_destroy(ctx);
     return 0;
 }
 
 
 int main() {
-    OpenSSL_add_all_algorithms();
-    ERR_load_crypto_strings();
+    // OpenSSL_add_all_algorithms();
+    // ERR_load_crypto_strings();
     return generate_ecdsa_keypair();
 }
 
